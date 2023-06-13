bdce044a23142be83348356cab326ed1c91aba25
tsxepo <tsxepo@gmail.com>
2023-06-13 14:36:27 +0200

    Add project files.

---
+++ Trivia.DataAccess/Entities/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia.DataAccess.Entities
+{
+    internal class Question
+    {
+    }
+}
