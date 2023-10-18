022a72a74eead098dc0f0a43f2116bffb02e7d5e
tsxepo <tsxepo@gmail.com>
2023-10-18 16:20:58 +0200

    Update environment env

--- Trivia.API/Program.cs
+++ Trivia.API/Program.cs
@@ -6,13 +6,10 @@ using Trivia.DataAccess.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var mongoConnectionString = builder.Configuration["Trivia:ConnectionString"];
+var mongoDatabaseName = builder.Configuration["Trivia:DatabaseName"];
+var mongoCollectionName = builder.Configuration["Trivia:CollectionName"];
 
-DotEnv.Load();
-var envKeys = DotEnv.Read();
-var mongoConnectionString = "mongodb://trivia-app:9Oqvib8Namzi5UO7ec4vunXZZynfyoD5HRwiogvZKrm1ITuZAWiDA2DbV1VSNiLbj48ALkfvSJylACDbzljhTw%3D%3D@trivia-app.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@trivia-app@";
-var mongoDatabaseName = "trivia";
-var mongoCollectionName = "questions";
 var mongoClient = new MongoClient(mongoConnectionString);
 var mongoDatabase = mongoClient.GetDatabase(mongoDatabaseName);
 var mongoCollection = mongoDatabase.GetCollection<Questions>(mongoCollectionName);
@@ -25,7 +22,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
